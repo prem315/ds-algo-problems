92093bfcdb70d5c4752d73cfaaee02dab4eb16e2
prem315 <prem.patel3105@gmail.com>
2021-06-17 22:59:00 +0530

    length of the longest substring in it with no more than K distinct characters.

---
+++ slidingWindow/challenge4.js
@@ -0,0 +1,33 @@
+// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+function longestSubStr(str, k) {
+    // console.log(str)
+    let sliding_window_index = 0
+    let maxLength = 0
+    let charCount = {}
+    for(let i=0; i<str.length; i++){
+        
+        if(!(str[i] in charCount)) {
+            charCount[str[i]] = 0
+        } 
+        charCount[str[i]] = charCount[str[i]] + 1
+
+        if(Object.keys(charCount).length === k) {
+            maxLength = Math.max(maxLength, i - sliding_window_index + 1)
+        }
+        
+        if(Object.keys(charCount).length > k) {
+            while(Object.keys(charCount).length > k) {
+                charCount[str[i]] = charCount[str[i]] - 1
+                if(charCount[str[i]] === 0) {
+                    delete charCount[str[i]]
+                }  
+                sliding_window_index = sliding_window_index + 1;
+            }
+        }
+    }
+
+    console.log(maxLength)
+}
+
+longestSubStr('araaci', 2)
