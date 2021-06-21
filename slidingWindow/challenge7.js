3ba09d4199d073b22e633e03958d1b1708e98e33
prem315 <prem.patel3105@gmail.com>
2021-06-21 21:27:32 +0530

    Longest Substring with Same Letters after Replacement

--- slidingWindow/challenge7.js
+++ slidingWindow/challenge7.js
@@ -2,6 +2,7 @@ function subStrwithSameCharsAfterReplacement(str, k) {
 
     let sliding_window_index = 0;
     let maxLength = 0;
+    let maxRepeatedCharInWindow = 0;
     let counter = {}
 
     for(let i=0; i<str.length; i++) {
@@ -13,9 +14,20 @@ function subStrwithSameCharsAfterReplacement(str, k) {
 
         counter[char] = counter[char] + 1
 
-        
+        maxRepeatedCharInWindow = Math.max(maxRepeatedCharInWindow, counter[char])
+
+        if(i - sliding_window_index + 1 - maxRepeatedCharInWindow > k) {
+            let leftChar = str[sliding_window_index]
+            counter[leftChar] = counter[leftChar] - 1
+            sliding_window_index = sliding_window_index + 1
+        }
+
+        maxLength = Math.max(maxLength, i - sliding_window_index + 1)
     }
 
+    console.log(maxLength)
+    return maxLength
 }
 
-subStrwithSameCharsAfterReplacement("aabccbb", 2) 
+subStrwithSameCharsAfterReplacement("bccbababd", 2) 
+
