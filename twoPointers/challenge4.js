ec1bba8c2a08241d0a9b19fdd0d998fbfca28ffc
prem315 <prem.patel3105@gmail.com>
2021-06-24 22:14:55 +0530

    triple sum 0

---
+++ twoPointers/challenge4.js
@@ -0,0 +1,39 @@
+function search_triplets(arr) {
+    arr.sort((a, b) => a - b);
+    const triplets = [];
+    for (i = 0; i < arr.length; i++) {
+        if (i > 0 && arr[i] === arr[i - 1]) { 
+            continue;
+        }
+        search_pair(arr, -arr[i], i + 1, triplets);
+    }
+  
+    return triplets;
+}
+  
+  
+function search_pair(arr, target_sum, left, triplets) {
+    let right = arr.length - 1;
+    while (left < right) {
+        const current_sum = arr[left] + arr[right];
+        if (current_sum === target_sum) { 
+            triplets.push([-target_sum, arr[left], arr[right]]);
+            left += 1;
+            right -= 1;
+            while (left < right && arr[left] === arr[left - 1]) {
+                left += 1; 
+            }
+            while (left < right && arr[right] === arr[right + 1]) {
+                right -= 1; 
+            }
+        } else if (target_sum > current_sum) {
+            left += 1; 
+        } else {
+            right -= 1; 
+        }
+    }
+}
+  
+  
+search_triplets([-3, 0, 1, 2, -1, 1, -2])
+search_triplets([-5, 2, -1, -2, 3])
